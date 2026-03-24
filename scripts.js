c67acc1c52063ba379ce540999a5d9d4dca9fcb3
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-03-23 23:16:52 -0300

    style: ajusta layout e botões

---
+++ scripts.js
@@ -0,0 +1,24 @@
+/*
+    Lógica de Programação
+    - Algoritimo (Receita de bolo - passso a passo)
+
+    Fluxo básico 
+
+    [] Descobrir quando o botão foi clicado
+    [] Pegar o nome da cidade do input
+    [] Enviar a Cidade para o servidor
+    [] Pegar a resposta e colocar na tela
+
+    Fluxo de voz
+    [] Descobrir quando o botão foi clicado
+    [] Comecar a ouvir e pegar a trancricao
+    [] Enviar trancricao para o servidor
+    [] Pegar a resposta e coplocar na tela
+
+    Fluxo da IA
+    [] Pegar os dados da cidade
+    [] Enviar dados para IA
+    [] Colocar os dados na tela
+*/
+
+
